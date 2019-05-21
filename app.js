5e64db37b073bdbdc99ce7c4b903d68bea7ee89c
bfly2001 <brendan.sflynn@gmail.com>
2019-05-21 11:04:43 -0700

    added phrases array object

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 //declared variables
-const qwerty = document.getElementById('qwerty');
-const phrase = document.getElementById('phrase');
+const qwerty = document.getElementById("qwerty");
+const phrase = document.getElementById("phrase");
 let missed = 0;
 
-//addition of event listener to button
+//event listener to button
 document.getElementsByClassName("btn__reset").addEventListener("click", function(){
   document.getElementById("overlay").style.display = "none";
 });
+
+// phrases array object
+var phrases = ["see eye to eye", "speak of the devil", "when pigs fly",
+ "once in a blue moon", "piece of cake"];
